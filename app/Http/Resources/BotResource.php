6926351f4f200ba3899a9ff19ac5272fc7a34aaa
514sid <140138716+514sid@users.noreply.github.com>
2023-11-02 00:01:16 +0400

    remove wrapper

--- app/Http/Resources/BotResource.php
+++ app/Http/Resources/BotResource.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BotResource extends JsonResource
 {
+	public static $wrap = null;
+
     public function toArray(Request $request): array
     {
         return [
