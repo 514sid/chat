128d6bf0219c1b114f102b3297a6e85fbaa332ab
514sid <140138716+514sid@users.noreply.github.com>
2023-11-01 22:55:20 +0400

    add models

---
+++ app/Models/Bot.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Chat;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Bot extends Model
+{
+    use HasFactory;
+
+	protected $table = 'bots';
+
+	protected $fillable = [
+        'name',
+        'token',
+        'offset',
+        'username',
+        'description',
+        'short_description',
+		'updates_retrieved_at',
+    ];
+
+	protected $casts = [
+        'updates_retrieved_at' => 'datetime',
+    ];
+
+	public function chats(): HasMany
+    {
+		return $this->hasMany(Chat::class);
+    }
+}
