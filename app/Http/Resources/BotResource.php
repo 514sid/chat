348b6e13ccdc912ceef9e9efbcf611004075b5af
514sid <140138716+514sid@users.noreply.github.com>
2023-11-01 23:04:30 +0400

    add bot json resource

---
+++ app/Http/Resources/BotResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BotResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+			'name'                 => $this->name,
+			'username'             => $this->username,
+			'description'          => $this->description,
+			'short_description'    => $this->short_description,
+			'updates_retrieved_at' => $this->updates_retrieved_at,
+		];
+    }
+}
