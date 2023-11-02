54ecb58fc19c6bfa94661a2e3376af5f737a76e4
514sid <140138716+514sid@users.noreply.github.com>
2023-11-02 12:44:52 +0400

    add react, ts, tailwind, eslint

---
+++ resources/js/main.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App"
+import "../css/app.css"
+
+const app = ReactDOM.createRoot(
+	document.getElementById("app") as HTMLElement
+)
+
+app.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode >
+)
