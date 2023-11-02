dbdab4fcb486d891ced44d3e2c4664d0c11ce52c
514sid <140138716+514sid@users.noreply.github.com>
2023-11-02 23:10:44 +0400

    add chat status updates table

---
+++ database/migrations/2023_01_01_000006_create_chat_status_updates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Chat;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_status_updates', function (Blueprint $table) {
+            $table->id();
+			$table->string('status');
+			$table->foreignIdFor(Chat::class)->index()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_status_updates');
+    }
+};
