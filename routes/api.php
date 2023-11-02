d8e0ffc71e1cffad76dc53adc94ddddb038005f4
514sid <140138716+514sid@users.noreply.github.com>
2023-11-03 01:23:24 +0400

    add get chats route

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BotController;
+use App\Http\Controllers\ChatController;
 
 Route::post('/bot', [BotController::class, 'create']);
+
+Route::get('/chats', [ChatController::class, 'get']);
 
 Route::get('/ping', function () {
     return response()->json('pong', 200);
