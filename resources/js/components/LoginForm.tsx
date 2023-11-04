39c8e22a049537a2b6acdaad3c660840c3e7e7f9
514sid <140138716+514sid@users.noreply.github.com>
2023-11-04 21:13:28 +0400

    add validation errors output

--- resources/js/components/LoginForm.tsx
+++ resources/js/components/LoginForm.tsx
@@ -1,10 +1,10 @@
+import { useEffect } from "react"
+import { LoginData } from "@/types/types"
 import { StringInput } from "@/components/inputs"
 import { BaseButton } from "@/components/buttons"
-import { useLoginMutation } from "@/api/mutations/authMutations"
-import { SubmitHandler, useForm } from "react-hook-form"
-import { LoginData } from "@/types/types"
-import { useEffect } from "react"
 import { useLaravelValidationErrors } from "@/hooks"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { useLoginMutation } from "@/api/mutations"
 
 const CreateRootCommand = ({ isVisible }: { isVisible: boolean }) => {
 	if (isVisible) {
@@ -34,7 +34,7 @@ export const LoginForm = () => {
 		if(validationErrors) {
 			Object.keys(validationErrors).forEach((field) => {
 				setError(field as keyof LoginData, {
-					type: "manual",
+					type: "server",
 					message: validationErrors[field][0],
 				})
 			})
@@ -44,15 +44,17 @@ export const LoginForm = () => {
 	return (
 		<div className="w-[336px]">
 			<form onSubmit={handleSubmit(onSubmit)}>
-				<div className="grid grid-cols-1 gap-5">
+				<div className="grid grid-cols-1 gap-2">
 					<StringInput
 						type="text"
 						placeholder="Username"
+						error={errors.username?.message}
 						{...register("username")}
 					/>
 					<StringInput
 						type="password"
 						placeholder="Password"
+						error={errors.password?.message}
 						{...register("password")}
 					/>
 					<BaseButton>
--- resources/js/components/inputs/StringInput.tsx
+++ resources/js/components/inputs/StringInput.tsx
@@ -4,23 +4,30 @@ type StringInputType = "text" | "password" | "email";
 
 interface ComponentProps extends InputHTMLAttributes<HTMLInputElement> {
 	type?: StringInputType;
+	error?: string;
 }
 
 export const StringInput = forwardRef<HTMLInputElement, ComponentProps>(
 	(
 		{
 			type = "text",
+			error,
 			...rest
 		},
 		ref
 	) => {
 		return (
-			<input
-				ref={ref}
-				type={type}
-				className="h-12 px-3 block w-full border border-gray-200 rounded text-sm focus:border-blue-500 focus:ring-blue-500 focus:outline-none"
-				{...rest}
-			/>
+			<div>
+				<input
+					ref={ref}
+					type={type}
+					className="h-12 px-3 block w-full border border-gray-200 rounded text-sm focus:border-blue-500 focus:ring-blue-500 focus:outline-none"
+					{...rest}
+				/>
+				<div className="mt-1 text-sm text-red-600 h-6">
+					{error}
+				</div>
+			</div>
 		)
 	}
 )
