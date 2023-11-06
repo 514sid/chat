4498b4f584ef586a3035fa6b5b271c7b51027d49
514sid <140138716+514sid@users.noreply.github.com>
2023-11-06 22:30:10 +0400

    add latest update

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -9,18 +9,19 @@ class ChatResource extends JsonResource
 {
 	public static $wrap = null;
 
-	public static $relationships = ['bot'];
+	public static $relationships = ['bot', 'latestUpdate'];
 
     public function toArray(Request $request): array
     {
         return [
-			'id'         => $this->id,
-			'status'     => $this->status,
-			'username' 	 => $this->username,
-			'first_name' => $this->first_name,
-			'last_name'  => $this->last_name,
-			'bot'        => new BotResource($this->bot),
-			'created_at' => $this->created_at,
+			'id'            => $this->id,
+			'status'        => $this->status,
+			'username'      => $this->username,
+			'first_name'    => $this->first_name,
+			'last_name'     => $this->last_name,
+			'bot'           => new BotResource($this->bot),
+			'created_at'    => $this->created_at,
+			'latest_update' => new ChatHistoryItemResource($this->latestUpdate),
 		];
     }
 }
